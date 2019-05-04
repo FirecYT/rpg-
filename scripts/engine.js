7b3f20ceeb4a4e729d4d1718843aebc0c3b34822
Firec <kratos150@yandex.ru>
2019-05-05 01:22:29 +0300

    U gay

--- scripts/engine.js
+++ scripts/engine.js
@@ -1,15 +1,27 @@
-var rect = function(x, y, width, height, colorConst, clolorActive){
+var rect = function(x, y, width, height, colorConst, colorActive, colorClicked){
 	this.x = x;
 	this.y = y;
 	this.width = width;
 	this.height = height;
-	this.ative = 0;
+
+	this.active = 0;
 	this.colorConst = colorConst;
-	this.clolorActive = clolorActive || colorConst;
+	this.colorActive = colorActive || colorConst;
+	this.colorClicked = colorClicked || "#AFA";
 }
 rect.prototype.draw = function(canvas) {
 	ctx = canvas.getContext('2d');
-	ctx.fillStyle = this.active?this.clolorActive:this.colorConst;
+	switch(this.active){
+		case 0:
+			ctx.fillStyle = this.colorConst;
+			break;
+		case 1:
+			ctx.fillStyle = this.colorActive;
+			break;
+		case 2:
+			ctx.fillStyle = this.colorClicked;
+			break;
+	}
 	ctx.fillRect(this.x, this.y, this.width, this.height);
 };
 
@@ -25,7 +37,7 @@ var title = function (src, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight) {
 	this.dHeight = dHeight;
 
 	this.image = new Image();
-	this.image.src = src;
+	this.image.src = "./images/"+src;
 }
 title.prototype.draw = function(canvas) {
 	ctx = canvas.getContext('2d');
@@ -33,21 +45,39 @@ title.prototype.draw = function(canvas) {
 	ctx.fillRect(this.x, this.y, this.width, this.height);
 };
 
-var sprite = function (src, x, y, color) {
+var sprite = function (src, x, y, colorConst, colorActive, colorClicked) {
 	this.x = x;
 	this.y = y;
-	this.color = color;
+
+	this.active = 0; // 0 - просто. 1 - навели курсором. 2 - нажали
+	this.colorConst = colorConst || "#00000000";
+	this.colorActive = colorActive || colorConst;
+	this.colorClicked = colorClicked || "#AFA";
+
 	this.image = new Image();
-	this.image.src = src;
+	this.image.src = "./images/"+src;
 }
 sprite.prototype.draw = function(canvas) {
 	ctx = canvas.getContext('2d');
-	ctx.fillStyle = this.color || "#00000000";
+	switch(this.active){
+		case 0:
+			ctx.fillStyle = this.colorConst;
+			break;
+		case 1:
+			ctx.fillStyle = this.colorActive;
+			break;
+		case 2:
+			ctx.fillStyle = this.colorClicked;
+			break;
+	}
 	ctx.fillRect(this.x, this.y, this.image.width, this.image.height);
 	ctx.drawImage(this.image, this.x, this.y);
 };
+sprite.prototype.setSRC = function(src) {
+	this.image.src = "./images/"+src;
+};
 
-clear = function(canvas) {
+var clear = function(canvas) {
 	ctx = canvas.getContext('2d');
 	ctx.clearRect(0, 0, 512, 512);
 };
@@ -57,6 +87,10 @@ var text = function(canvas, text, x, y, color, font) {
 	ctx.fillStyle = color;
 	ctx.font = font || "8px Consolas";
 	ctx.fillText(text, x, y);
+}
+
+var generateRect = function(obj) {
+	return {x: obj.x, y: obj.y, w: obj.width || obj.image.width, h: obj.height || obj.image.height}
 }
 
 var colis = function(mouse, object) { // mouse = {x, y} object = {x, y, w, h}
--- scripts/game.js
+++ scripts/game.js
@@ -2,38 +2,56 @@ start_cnv = doc[getId]("start");
 game_cnv = doc[getId]("game");
 gui_cnv = doc[getId]("gui");
 
-// Курсор для красоты
-cursor = new rect(0, 0, 4, 4, "#FFFFFF66");
-
 // Старт игры. Точнее выбор класса.
 start = [new rect(0, 0, 512, 512, "#666"),new rect(0, 0, 512, 32, "#333")] // Фон и полоска сверху
 classes = [ // Классы
 	new rect(32, 64, 128, 128, "#777", "#555"), new rect(192, 64, 128, 128, "#777", "#555"), new rect(352, 64, 128, 128, "#777", "#555"),
-	new rect(32, 224, 128, 128, "#777", "#555"), new sprite("./lock.png", 192, 224, "#700"), new sprite("./lock.png", 352, 224, "#700")
+	new rect(32, 224, 128, 128, "#777", "#555"), new sprite("lock.png", 192, 224, "#700"), new sprite("lock.png", 352, 224, "#700")
 ]
 enter = new rect(192, 416, 128, 64, "#777", "#555") // Подтверждение выбора
 
+secrets = {
+	loli: new sprite("loliv0.png", 192, 224, "#777", "#555") // Просто подгрузка, я не буду это использовать
+}
+
 gui_cnv.addEventListener("mouseup", (e)=>{ // Кнопка мыши была поднята (вроде бы и ПКМ и ЛКМ)
-	console.log(colis({x: e.layerX, y: e.layerY}, {x: enter.x, y: enter.y, w: enter.width, h: enter.height})); // DEBUG
+	cX = e.layerX; // Я не индус, буду дулать код короче
+	cY = e.layerY;
+
+	for(i in classes){ // Отслеживание пересечение курсора с каким нибудь классом
+		if ( colis({x: cX, y: cY}, generateRect(classes[i])) ) {
+			if( classes[i].image ) {
+				if ( classes[i].image.src.split("/")[10]=="lock.png")
+					break; 
+			}
+			for(j in classes){
+				classes[j].active = 0;
+			}
+			classes[i].active = 2;
+		}
+		classes[i].draw(start_cnv); // Ну и рисуем, чтобы увидеть результат
+	}
+
+	if( colis({x: e.layerX, y: e.layerY}, generateRect(classes[4])) ) { // Лоли 2.0
+		classes[4] = secrets.loli;
+		classes[4].draw(start_cnv);
+	}
 })
 gui_cnv.addEventListener("mousemove", (e)=>{ // Движение курсора
 	cX = e.layerX; // Я не индус, буду дулать код короче
 	cY = e.layerY;
 
-	clear(gui_cnv); // Очищаю слой для курсора
-	cursor.x=cX-cursor.width/2; // Выравниваю по центру
-	cursor.y=cY-cursor.height/2;
-	cursor.draw(gui_cnv); // Рисую курсор
-
 	for(i in classes){ // Отслеживание пересечение курсора с каким нибудь классом
-		if ( colis({x: cX, y: cY}, {x: classes[i].x, y: classes[i].y, w: classes[i].width, h: classes[i].height}) ) {
+		if ( classes[i].active==2 ) {
+
+		} else if ( colis({x: cX, y: cY}, generateRect(classes[i])) ) {
 			classes[i].active=1; // Если пересёкся, то делаем его активным
 		} else { // Иначе делаем не активным (нам же нужно вернуть цвет)
 			classes[i].active=0;
 		}
 		classes[i].draw(start_cnv); // Ну и рисуем, чтобы увидеть результат
 	}
-	if( colis({x: cX, y: cY}, {x: enter.x, y: enter.y, w: enter.width, h: enter.height}) ){ // То же самое с enter'ом
+	if( colis({x: cX, y: cY}, generateRect(enter)) ){ // То же самое с enter'ом
 		enter.active=1;
 	} else {
 		enter.active=0;
