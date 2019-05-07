005ff2a6b153d006719baecbccbae669f4f24d8b
Firec <kratos150@yandex.ru>
2019-05-07 06:55:58 +0300

    Update 0.0.8.2 "Good code"

--- scripts/game.js
+++ scripts/game.js
@@ -11,10 +11,46 @@ var map = [	[0,0,0,0,0,0,0,0],
 var id = [
 	{type: "floor", img: new title("title.png",0,0,16,16)},
 	{type: "player", img: new title("title.png",16,0,16,16)},
-	{type: "wall", img: new title("title.png",0,16,16,16)}
+	{type: "box", img: new title("title.png",0,16,16,16)}
 ]
 
-let size = 64;
+var size = 64;
+
+
+
+let tick = function(e) {
+	let cX = e.layerX; // Я не индус, буду дулать... А-а-а!!!
+	let cY = e.layerY;
+
+	move(cX, cY);
+
+	drawMap();
+}
+
+var startGame = function(){
+	mouse_cnv.removeEventListener("mouseup", mouseup);
+	mouse_cnv.addEventListener("mouseup", tick);
+
+	for(let y in map) {
+		for(let x in map[y]){
+			if (map[y][x]==0) {
+				if (randomInt(100)<=15) map[y][x]=2;
+			}
+		}
+	}
+
+	drawMap();
+}
+
+
+
+// Далее идут какие-то функции. Я бы сказал вспомогательные, но с ними возишься дольше.
+let canGo = function(x, y) { // Особенно с этой
+	if(map[y][x]!=0){return false;} // Не переместиться в стенку
+	let pos = searchPlayer();
+	if( Math.abs(x-pos[0]) <= 1 && Math.abs(y-pos[1]) <= 1) return true;
+	return false;
+}
 
 let drawMap = function() {
 	for(let y in map) {
@@ -23,17 +59,6 @@ let drawMap = function() {
 			id[map[y][x]].img.draw(game_cnv, x*size, y*size, size, size);
 		}
 	}
-}
-
-let tick = function() {
-	drawMap();
-}
-
-let canGo = function(x, y) {
-	if(map[y][x]!=0){return false;} // Не переместиться в стенку
-	let pos = searchPlayer();
-	if( Math.abs(x-pos[0]) <= 1 && Math.abs(y-pos[1]) <= 1) return true;
-	return false;
 }
 
 let searchPlayer = function() {
@@ -46,36 +71,17 @@ let searchPlayer = function() {
 	}
 }
 
-var startGame = function(){
-	mouse_cnv.removeEventListener("mouseup", mouseup);
-	mouseup = (e)=>{ // Кнопка мыши была поднята (вроде бы и ПКМ и ЛКМ)
-		let cX = e.layerX; // Я не индус, буду дулать код короче
-		let cY = e.layerY;
+let move = function(cX, cY) {
+	for(let y in map) {
+		for(let x in map[y]){
+			if( collision({x: cX, y: cY}, {x:x*size,y:y*size,w:size,h:size}) ){
+				if(canGo(x, y)){
+					let pos = searchPlayer();
 
-		for(let y in map) {
-			for(let x in map[y]){
-				if( collision({x: cX, y: cY}, {x:x*size,y:y*size,w:size,h:size}) ){
-					if(canGo(x, y)){
-						let pos = searchPlayer();
-
-						map[pos[1]][pos[0]]=0;
-						map[y][x]=1;
-					}
+					map[pos[1]][pos[0]]=0;
+					map[y][x]=1;
 				}
 			}
 		}
-
-		drawMap();
 	}
-	mouse_cnv.addEventListener("mouseup", mouseup);
-
-	for(let y in map) {
-		for(let x in map[y]){
-			if (map[y][x]==0) {
-				if (randomInt(100)<=15) map[y][x]=2;
-			}
-		}
-	}
-
-	drawMap();
 }
